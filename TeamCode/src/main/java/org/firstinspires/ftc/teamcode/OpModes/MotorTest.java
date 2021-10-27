e391594f4f9aefa8d211f21f2fda43dfecac65da
MasonI2006 <93228033+MasonI2006@users.noreply.github.com>
2021-10-26 19:24:17 -0500

    Add motor test opmode
    Create an autonomous opmode that drives each motor one at a time.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/MotorTest.java
@@ -0,0 +1,42 @@
+package org.firstinspires.ftc.teamcode.OpModes;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+
+import org.firstinspires.ftc.teamcode.APIs.DriveApi;
+
+@Autonomous(name="MotorTest")
+public class MotorTest extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        // Code to run when opmode is initialized goes here
+
+        DcMotor frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
+        DcMotor frontRight = hardwareMap.get(DcMotor.class, "frontRight");
+        DcMotor rearLeft = hardwareMap.get(DcMotor.class, "rearLeft");
+        DcMotor rearRight = hardwareMap.get(DcMotor.class, "rearRight");
+
+        DriveApi drivetrain = new DriveApi(frontLeft, frontRight, rearLeft, rearRight, this);
+
+        waitForStart();
+
+        drivetrain.driveFrontLeft(0.1);
+        sleep(3000);
+        drivetrain.stopMotors();
+        drivetrain.driveFrontRight(0.1);
+        sleep(3000);
+        drivetrain.stopMotors();
+        drivetrain.driveRearLeft(0.1);
+        sleep(3000);
+        drivetrain.stopMotors();
+        drivetrain.driveRearRight(0.1);
+        sleep(3000);
+        drivetrain.stopMotors();
+
+    }
+
+}
