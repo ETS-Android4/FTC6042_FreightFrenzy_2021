4fc7fc694f522f9250a8e436288b1f60eb938d1c
IsaacIngram22 <iingram@students.casciahall.com>
2021-11-11 14:04:36 -0600

    Add delivery mechanism

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Tests/EncoderValuePrintout.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Tests/EncoderValuePrintout.java
@@ -1,11 +1,12 @@
 package org.firstinspires.ftc.teamcode.OpModes.Tests;
 
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.teamcode.mechanisms.DeliveryWheel;
 import org.firstinspires.ftc.teamcode.mechanisms.Drivetrain;
 
-@Autonomous(name="Encoder value printout")
+@TeleOp(name="Encoder value printout")
 public class EncoderValuePrintout extends LinearOpMode {
 
     @Override
@@ -13,6 +14,8 @@ public class EncoderValuePrintout extends LinearOpMode {
 
         Drivetrain drivetrain = new Drivetrain();
         drivetrain.init(this);
+        DeliveryWheel deliveryWheel = new DeliveryWheel();
+        deliveryWheel.init(this);
 
         waitForStart();
 
@@ -21,6 +24,8 @@ public class EncoderValuePrintout extends LinearOpMode {
             telemetry.addLine("Front Right: " + drivetrain.getFrontRightEncoderValue());
             telemetry.addLine("Rear Left: " + drivetrain.getRearLeftEncoderValue());
             telemetry.addLine("Rear Right: " + drivetrain.getRearRightEncoderValue());
+            telemetry.addLine("Delivery Left: " + deliveryWheel.getLeftPosition());
+            telemetry.addLine("Delivery Right: " + deliveryWheel.getRightPosition());
             telemetry.update();
         }
 
