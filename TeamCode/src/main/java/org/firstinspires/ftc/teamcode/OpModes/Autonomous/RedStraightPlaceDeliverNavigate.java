04bd4862597858f49a3a05c7a9830fdc4ac392fc
IsaacIngram22 <iingram@students.casciahall.com>
2021-11-30 18:10:00 -0600

    Create new autons for placing
    Create new autons for placing. These are only for the red side, so we'll have to copy these and invert the turns for the blue side.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Autonomous/RedStraightPlaceDeliverNavigate.java
@@ -0,0 +1,61 @@
+package org.firstinspires.ftc.teamcode.OpModes.Autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.APIs.AutonomousActions;
+import org.firstinspires.ftc.teamcode.APIs.TelemetryWriter;
+import org.firstinspires.ftc.teamcode.mechanisms.Armdex;
+import org.firstinspires.ftc.teamcode.mechanisms.DeliveryWheel;
+import org.firstinspires.ftc.teamcode.mechanisms.Drivetrain;
+
+@Autonomous(name="Red Place Straight Deliver Warehouse")
+public class RedStraightPlaceDeliverNavigate extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        Drivetrain drivetrain = new Drivetrain();
+        drivetrain.init(this);
+        Armdex armdex = new Armdex();
+        armdex.init(this);
+        DeliveryWheel deliveryWheel = new DeliveryWheel();
+        deliveryWheel.init(this);
+        AutonomousActions actions = new AutonomousActions();
+        actions.init(this);
+        TelemetryWriter output = new TelemetryWriter().setDrivetrain(drivetrain).setArmdex(armdex).setDeliveryWheel(deliveryWheel);
+        output.init(telemetry);
+        output.robotInitialized();
+
+        waitForStart();
+
+        // Place block
+        actions.placeFreightStraightFromWall();
+        actions.delay(500);
+
+        // Back into wall
+        actions.driveInchesNoPid(-20, 0.4);
+        actions.delay(500);
+
+        actions.rotateLeftFromWall();
+        actions.delay(500);
+
+        // Drive to wheel
+        drivetrain.driveForwardInchesNoPid(15, 0.5);
+        long startTimeInMillis = System.currentTimeMillis();
+        while(opModeIsActive() && (System.currentTimeMillis() < startTimeInMillis+1500)) {
+            drivetrain.driveAtPower(0.2);
+        }
+        drivetrain.stopMotors();
+
+        // Deliver
+        actions.deliverDuckAutonomous();
+        actions.delay(1000);
+
+        // Drive back to warehouse
+        actions.parkInWarehouseFromCarouselBackwards();
+
+        armdex.wristUp();
+
+    }
+}
