2ef687bc91cb9bbea13298180199fea62f5dd144
IsaacIngram22 <iingram@students.casciahall.com>
2021-11-30 18:08:55 -0600

    Initial commit
    Create a teleop mode that we can use to test the intake and wrist without any color sensor and other mechanisms. We used this to try and shoot freight into the second level of the alliance specific shipping hub, which is wy it is named as it is.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Tests/ShooterTest.java
@@ -0,0 +1,49 @@
+package org.firstinspires.ftc.teamcode.OpModes.Tests;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.APIs.TelemetryWriter;
+import org.firstinspires.ftc.teamcode.mechanisms.Armdex;
+import org.firstinspires.ftc.teamcode.mechanisms.Drivetrain;
+
+@TeleOp(name="Shooter Test")
+@Disabled
+public class ShooterTest extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        Drivetrain drivetrain = new Drivetrain();
+        drivetrain.init(this);
+        Armdex armdex = new Armdex();
+        armdex.init(this);
+        TelemetryWriter output = new TelemetryWriter().setDrivetrain(drivetrain).setArmdex(armdex).init(telemetry);
+
+        output.robotInitialized();
+
+        waitForStart();
+
+        while(opModeIsActive()) {
+
+            if(gamepad2.right_trigger > 0.2) {
+                armdex.setIntakePower(1);
+            } else if(gamepad2.left_trigger > 0.2) {
+                armdex.setIntakePower(-1);
+            } else {
+                armdex.stopIntake();
+            }
+
+            if(gamepad2.dpad_up) {
+                armdex.runWristUp();
+            } else if(gamepad2.dpad_down) {
+                armdex.runWristDown();
+            } else {
+                armdex.stopWrist();
+            }
+
+        }
+
+    }
+}
