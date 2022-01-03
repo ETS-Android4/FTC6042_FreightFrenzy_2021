3393c403c905d3de443574b21aae9849d733ce09
IsaacIngram22 <iingram@students.casciahall.com>
2022-01-03 00:08:45 -0600

    Add delay programs for HiveMind

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Autonomous/BluePlaceDeliverDelayWarehouse.java
@@ -0,0 +1,58 @@
+package org.firstinspires.ftc.teamcode.OpModes.Autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.APIs.AutonomousActions;
+import org.firstinspires.ftc.teamcode.APIs.Leds.LedController;
+import org.firstinspires.ftc.teamcode.APIs.TelemetryWriter;
+import org.firstinspires.ftc.teamcode.mechanisms.Armdex;
+import org.firstinspires.ftc.teamcode.mechanisms.DeliveryWheel;
+import org.firstinspires.ftc.teamcode.mechanisms.Drivetrain;
+
+@Autonomous(name="Blue PD-Delay-W")
+public class BluePlaceDeliverDelayWarehouse extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        Drivetrain drivetrain = new Drivetrain(this);
+        Armdex armdex = new Armdex(this);
+        DeliveryWheel deliveryWheel = new DeliveryWheel(this);
+        AutonomousActions actions = new AutonomousActions(this);
+        TelemetryWriter output = new TelemetryWriter(telemetry).setDrivetrain(drivetrain).setArmdex(armdex).setDeliveryWheel(deliveryWheel);
+        LedController led = new LedController(this, 'b');
+        output.robotInitialized();
+
+        waitForStart();
+
+        // Place block
+        actions.dropFreightOnLevelOne();
+        actions.delay(500);
+
+        // Back into wall
+        actions.driveInchesNoPid(-20, 0.4);
+        actions.delay(500);
+
+        actions.rotateRightFromWall();
+        actions.delay(500);
+
+        // Drive to wheel
+        drivetrain.driveForwardInchesNoPid(19.5, 0.5);
+        long startTimeInMillis = System.currentTimeMillis();
+        while(opModeIsActive() && (System.currentTimeMillis() < startTimeInMillis+2000)) {
+            drivetrain.driveAtPower(0.2);
+        }
+        drivetrain.stopMotors();
+
+        // Deliver
+        actions.deliverDuckAutonomous();
+        actions.delay(1000);
+
+        actions.delay(5000);
+
+        // Drive back to warehouse
+        actions.parkInWarehouseFromCarouselBackwards();
+
+        armdex.wristUp();
+    }
+}
